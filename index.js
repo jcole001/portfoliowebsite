749b3b1775aad6adac27529e04c71453283dbad2
jcole001 <jcoleharry84@gmail.com>
2024-09-09 12:12:20 +0300

    improve website

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-
-//add an eventlistener for the closing the menu: 
-
-
-navLink.forEach(n => n.addEventListener("click" , closeMenu))
-
-function closeMenu () {
-    hamburgerActive.classList.remove("active")
-    menupaste.classList.remove("active")
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
-
-
-
